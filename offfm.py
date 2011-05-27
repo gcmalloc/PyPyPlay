adb560b1a052ac1cf2d1becea169abb35a5c34f1
gcmalloc <gcmalloc@gmail.com>
2011-05-27 17:04:18 +0200

    a push for the sake of it, added some comment and made some method, still kind of pseudo code

--- offfm.py
+++ offfm.py
@@ -2,33 +2,73 @@ import urllib, json
 from random import randint
 from os import remove, path
 from math import floor
+from urlparse import urlparse
 
-http://api.official.fm/search/tracks/{searchparam}?api_max_responses=20
+MIN_SEARCH_SIZE = 3
+BASE_URL = http://api.official.fm
 "this class is just a way to handle the url we use in all the instance"
-class OffFm:
-	url_base="https://api.official.fm"
-	url_format="&format=json"
-	search_url=""
-	
-	
-class OffApi(OffFm):
-	url_maxresponse="api_max_responses="
-	url_search_playlist="search/playlists/"
+class OffFm ():	
+class OffApi():
 	"this class gives all the way to access official fm"
 	"the default key is the devlopper key"
 	"the default user is set to null for no binded user"
-	def __init__(self, key=lBZLgyJlImbRf529dTVS, user=null):
+	def __init__(self, key=lBZLgyJlImbRf529dTVS, option=null,user=null):
 		self.key = key
 		self.user = user
 		return
-	
-	def search_track(self, search_param, max_response=20):
 		
-	def search_user(self, search_param, max_response=20):
-	def latest_track(self):
-	def charting_track(self):
+	"this method does the following request"	
+	"http://api.official.fm/search/tracks/{searchparam}?api_max_responses=20"
+	"return a list of 20 Track"
+	def search_track(self, search_param, max_response=20):
+		url = urllib.parse(urlbase + search param + track + options)
+		answer = get_json(url)
+		for json in answer:
+			yield Track(json) 
 	
+	"this method does the following request"	
+	"http://api.official.fm/search/user/{searchparam}?api_max_responses=20"
+	"return a list of 20 users"
+	def search_user(self, search_param, max_response=20):
+		url = urllib.parse(urlbase + search param + track + options)
+		answer = get_json(url)
+		for json in answer:
+			yield User(json) 
+	
+	"this method does the following request"	
+	"http://api.official.fm/search/user/{searchparam}?api_max_responses=20"
+	"return a list of 20 playlist"
+	def search_playlist(self, search_param, max_response=20):
+		url = urllib.parse(urlbase + search param + track + options)
+		answer = get_json(url)
+		for json in answer:
+			yield Playlist(json) 
+	
+	"http://api.official.fm/tracks/latest"
+	def latest_track(self):
+		url 
+		answer = get_json(url)
+		return Track(answer)
+		
+	"http://api.official.fm/tracks/charts?charting=today"
+	def charting_track(self, time):
+		
 
+
+"instanciate a simple object from a parsed json object"
+class JsonObject:
+	def __init__(self, json_descriptor):
+		for k, v in json_descriptor.items():
+			settart(self, k, v)
+	
+	def get_json(self, url):
+		
+class Track(JsonObject, OffFm):
+	"define a song with the field given by official.fm"
+	"return a stream on the audio file"
+	"initialise a track object"
+	def __init__(self):
+		
 	def get_value(self, key):
 		ret = []
 		for i in self.data:
@@ -42,21 +82,7 @@ class OffApi(OffFm):
 		return self.url_mp3[0] + direct + self.url_mp3[1] + str(id) + self.url_mp3[2] + self.gen_l()
 		
 	def end(self, file_path):
-		remove(file_path)
-
-"instanciate a simple object from a parsed json object"
-class JsonObject:
-	def __init__(self, json_descriptor):
-		for k, v in json_descriptor.items():
-			settart(self, k, v)
-		
-class Track(JsonObject, OffFm):
-	"define a song with the field given by official.fm"
-	"return a stream on the audio file"
-	"initialise a track object"
-	def __init__(self):
-		
-	
+		remove(file_path)	
 	"generate the magic key for the track"
 	def gen_magic_key():
 		ret = ""
@@ -99,7 +125,11 @@ class User(JsonObject, OffFm):
 	def get_voted_track(self):
 	def get_voted_Playlist(self):
 	def track_user(self):
-
-"return a json object ready to be parse from the url given"
-def return_json(url):
+		
+class Playlist(JsonObject, OffFm):
+	def __init(self):
+		
+	def show_playlist(self):
+		
+	def votes_playlist:
 	
